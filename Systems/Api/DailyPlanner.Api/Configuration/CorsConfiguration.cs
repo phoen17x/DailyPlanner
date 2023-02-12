ca9d4944bc6eaeacbd05769da8152785cb5977cc
phoen17x <olegerokhinp@gmail.com>
2023-02-12 19:00:50 +0300

    feat: add CORS

---
+++ Systems/Api/DailyPlanner.Api/Configuration/CorsConfiguration.cs
@@ -0,0 +1,35 @@
+﻿namespace DailyPlanner.Api.Configuration;
+
+/// <summary>
+/// The CORS configuration.
+/// </summary>
+public static class CorsConfiguration
+{
+    /// <summary>
+    /// Adds CORS services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The services collection instance</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddAppCors(this IServiceCollection services)
+    {
+        services.AddCors(builder =>
+        {
+            builder.AddDefaultPolicy(policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
+        });
+        return services;
+    }
+
+    /// <summary>
+    /// Uses CORS in the application.
+    /// </summary>
+    /// <param name="app">The application builder instance</param>
+    public static void UseAppCors(this IApplicationBuilder app)
+    {
+        app.UseCors();
+    }
+}
--- Systems/Api/DailyPlanner.Api/Program.cs
+++ Systems/Api/DailyPlanner.Api/Program.cs
@@ -5,10 +5,13 @@ builder.AddAppLogger();
 
 
 var services = builder.Services;
+services.AddHttpContextAccessor();
+services.AddAppCors();
 services.AddControllers();
 
 
 var app = builder.Build();
 app.UseAuthorization();
 app.MapControllers();
+app.UseAppCors();
 app.Run();
