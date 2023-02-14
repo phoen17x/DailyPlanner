d78488a50e73bab80b92a52b74be0159c1bc9b0b
phoen17x <olegerokhinp@gmail.com>
2023-02-14 21:52:36 +0300

    feat: add Swagger settings

---
+++ Services/DailyPlanner.Services.Settings/SwaggerSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace DailyPlanner.Services.Settings;
+
+/// <summary>
+/// Represents the settings for the Swagger.
+/// </summary>
+public class SwaggerSettings
+{
+    public bool Enabled { get; private set; }
+    public string? OAuthClientId { get; private set; }
+    public string? OAuthClientSecret { get; private set; }
+
+    public SwaggerSettings()
+    {
+        Enabled = false;
+    }
+}
