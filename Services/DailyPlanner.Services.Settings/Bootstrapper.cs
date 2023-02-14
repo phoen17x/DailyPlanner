a896cc9b8e36155c9a6affd4742942a5cebf443f
phoen17x <olegerokhinp@gmail.com>
2023-02-14 21:53:17 +0300

    feat: add settings bootstrapper

---
+++ Services/DailyPlanner.Services.Settings/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿namespace DailyPlanner.Services.Settings;
+
+using DailyPlanner.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Adds settings to the specified <see cref="IServiceCollection"/>.
+/// </summary>
+public static class Bootstrapper
+{
+    /// <summary>
+    /// Adds Swagger settings to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection instance.</param>
+    /// <param name="configuration">Optional configuration to load settings from.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddSwaggerSettings(this IServiceCollection services, IConfiguration? configuration = null)
+    {
+        var settings = Settings.Load<SwaggerSettings>("Swagger", configuration);
+        services.AddSingleton(settings!);
+
+        return services;
+    }
+}
