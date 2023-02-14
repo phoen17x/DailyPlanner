1ea7aa10a7c19200e5611e9f7681261241706106
phoen17x <olegerokhinp@gmail.com>
2023-02-14 21:54:53 +0300

    feat: add DI bootstrapper for app services

---
+++ Systems/Api/DailyPlanner.Api/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using DailyPlanner.Services.Settings;
+
+namespace DailyPlanner.Api;
+
+/// <summary>
+/// Responsible for registering application services in the dependency injection container.
+/// </summary>
+public static class Bootstrapper
+{
+    /// <summary>
+    /// Registers the application services in the dependency injection container.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection RegisterAppServices(this IServiceCollection services)
+    {
+        services
+            .AddSwaggerSettings();
+
+        return services;
+    }
+}
