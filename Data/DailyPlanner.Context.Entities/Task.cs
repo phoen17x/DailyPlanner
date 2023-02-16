4ce89dacf0d99464cc96a48a978e21c51f1f09fb
phoen17x <olegerokhinp@gmail.com>
2023-02-16 14:52:05 +0300

    feat: add TodoTask entity

---
+++ Data/DailyPlanner.Context.Entities/Task.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DailyPlanner.Context.Entities.Common;
+
+namespace DailyPlanner.Context.Entities;
+
+/// <summary>
+/// Represents a TodoTask entity.
+/// </summary>
+public class TodoTask : BaseEntity
+{
+    [Required]
+    public string Title { get; set; }
+    public string Description { get; set; }
+    [Required]
+    public DateTime StartTime { get; set;}
+    [Required]
+    public DateTime EstimatedCompletionTime { get; set; }
+    public DateTime ActualCompletionTime { get; set; }
+    [Required]
+    public TodoTaskStatus Status { get; set; }
+}
+
+/// <summary>
+/// Represents the status of a TodoTask.
+/// </summary>
+public enum TodoTaskStatus
+{
+    Scheduled,
+    Pending,
+    Completed,
+    Cancelled,
+    Deleted
+}
