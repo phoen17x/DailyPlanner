abb45076442e8ef314acb47f15cb2557d20cf867
phoen17x <olegerokhinp@gmail.com>
2023-02-16 14:28:16 +0300

    feat: add base entity class

---
+++ Data/DailyPlanner.Context.Entities/Common/BaseEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DailyPlanner.Context.Entities.Common;
+
+/// <summary>
+/// Base entity class with unique identifier and identity key.
+/// </summary>
+[Index("Uid", IsUnique = true)]
+public abstract class BaseEntity
+{
+    /// <summary>
+    /// Gets or sets the identity key.
+    /// </summary>
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public virtual int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the unique identifier.
+    /// </summary>
+    [Required]
+    public virtual Guid Uid { get; set; } = Guid.NewGuid();
+}
