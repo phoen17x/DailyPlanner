a8452583672bccbe798dbef639c40c4749942f42
phoen17x <olegerokhinp@gmail.com>
2023-02-17 11:25:08 +0300

    feat: add database settings

---
+++ Data/DailyPlanner.Context/Settings/DbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPlanner.Context.Settings;
+
+/// <summary>
+/// Represents the settings for the database.
+/// </summary>
+public class DbSettings
+{
+    public string ConnectionString { get; private set; }
+}
