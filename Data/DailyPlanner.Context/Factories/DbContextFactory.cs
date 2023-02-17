df86a1e83c27f3deee4c097e4e2b3e1a2c07bd4c
phoen17x <olegerokhinp@gmail.com>
2023-02-17 13:01:42 +0300

    feat: add DbContextFactory

---
+++ Data/DailyPlanner.Context/Factories/DbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DailyPlanner.Context.Factories;
+
+/// <summary>
+/// Factory for creating instances of <see cref="DailyPlannerContext"/>.
+/// </summary>
+public class DbContextFactory
+{
+    /// <summary>
+    /// The options to be used to configure the <see cref="DailyPlannerContext"/>.
+    /// </summary>
+    private readonly DbContextOptions<DailyPlannerContext> options;
+
+    public DbContextFactory(DbContextOptions<DailyPlannerContext> options)
+    {
+        this.options = options;
+    }
+
+    /// <summary>
+    /// Creates a new instance of the  <see cref="DailyPlannerContext"/> using the options provided to the factory.
+    /// </summary>
+    /// <returns>A new instance of  <see cref="DailyPlannerContext"/>.</returns>
+    public DailyPlannerContext Create() => new DailyPlannerContext(options);
+}
