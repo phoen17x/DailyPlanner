dd2f6544b8abe3c864ec9c5777c3bc3204ec7d41
phoen17x <olegerokhinp@gmail.com>
2023-02-17 13:08:27 +0300

    feat: add bootstrapper to register dbcontext

---
+++ Data/DailyPlanner.Context/Bootstrapper.cs
@@ -0,0 +1,31 @@
+﻿using DailyPlanner.Context.Factories;
+using DailyPlanner.Context.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DailyPlanner.Context;
+
+/// <summary>
+/// Registers database context.
+/// </summary>
+public static class Bootstrapper
+{
+    /// <summary>
+    /// Adds the database context to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the database context to.</param>
+    /// <param name="configuration">The optional <see cref="IConfiguration"/> instance to use for configuration settings.</param>
+    /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddAppDbContext(
+        this IServiceCollection services,
+        IConfiguration? configuration = null)
+    {
+        var settings = DailyPlanner.Settings.Settings.Load<DbSettings>("Database", configuration)!;
+        services.AddSingleton(settings);
+
+        var dbInitOptionsDelegate = DbContextOptionsFactory.Configure(settings.ConnectionString);
+
+        services.AddDbContextFactory<DailyPlannerContext>(dbInitOptionsDelegate);
+        return services;
+    }
+}
