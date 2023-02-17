74179e9dc85076499c40febb7db352aebaca8901
phoen17x <olegerokhinp@gmail.com>
2023-02-17 13:03:39 +0300

    feat: add DbContextOptionsFactory

---
+++ Data/DailyPlanner.Context/Factories/DbContextOptionsFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DailyPlanner.Context.Factories;
+
+/// <summary>
+/// Factory for creating <see cref="DbContextOptions"/> for <see cref="DailyPlannerContext"/>.
+/// </summary>
+public class DbContextOptionsFactory
+{
+    private const string migrationsAssembly = "DailyPlanner.Context.MigrationsPostgreSQL";
+
+    /// <summary>
+    /// Creates a new <see cref="DbContextOptions"/> for <see cref="DailyPlannerContext"/> with the specified connection string.
+    /// </summary>
+    /// <param name="connectionString">The connection string.</param>
+    /// <returns>A new <see cref="DbContextOptions"/>.</returns>
+    public static DbContextOptions<DailyPlannerContext> Create(string connectionString)
+    {
+        var builder = new DbContextOptionsBuilder<DailyPlannerContext>();
+        Configure(connectionString).Invoke(builder);
+        return builder.Options;
+    }
+
+    /// <summary>
+    /// Configures the <see cref="DbContextOptionsBuilder"/> with the specified connection string.
+    /// </summary>
+    /// <param name="connectionString">The connection string.</param>
+    /// <returns>An action that configures the <see cref="DbContextOptionsBuilder"/>.</returns>
+    public static Action<DbContextOptionsBuilder> Configure(string connectionString)
+    {
+        return builder =>
+        {
+            builder.UseNpgsql(connectionString, options =>
+            {
+                options
+                    .CommandTimeout((int)TimeSpan.FromMinutes(10).TotalSeconds)
+                    .MigrationsHistoryTable("__EFMigrationsHistory", "public")
+                    .MigrationsAssembly(migrationsAssembly);
+            });
+
+            builder
+                .EnableSensitiveDataLogging()
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        };
+    }
+}
