6751be396788a073b6bd88540fbf20d4206f9081
phoen17x <olegerokhinp@gmail.com>
2023-02-24 18:50:33 +0300

    feat: add ToCamelCase string extension method

---
+++ Shared/DailyPlanner.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace DailyPlanner.Common.Extensions;
+
+/// <summary>
+/// Provides extension methods for strings.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    /// Converts a string to camel case format, following the JSON naming policy.
+    /// </summary>
+    /// <param name="str">The string to convert.</param>
+    /// <returns>The camel case version of the input string.</returns>
+    public static string ToCamelCase(this string str)
+    {
+        return JsonNamingPolicy.CamelCase.ConvertName(str);
+    }
+}
