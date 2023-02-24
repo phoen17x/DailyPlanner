ece846cef690fabec7722065f8dc45c922612fac
phoen17x <olegerokhinp@gmail.com>
2023-02-24 18:41:02 +0300

    feat: add ErrorResponse class

---
+++ Shared/DailyPlanner.Common/Responses/ErrorResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace DailyPlanner.Common.Responses;
+
+/// <summary>
+/// Represents an error response that can be returned to the client in case of errors.
+/// </summary>
+public class ErrorResponse
+{
+    public int ErrorCode { get; set; }
+    public string Message { get; set; }
+    public IEnumerable<ErrorResponseFieldInfo> FieldErrors { get; set; }
+}
+
+/// <summary>
+/// Represents field information for a specific error response.
+/// </summary>
+public class ErrorResponseFieldInfo
+{
+    public string FieldName { get; set; }
+    public string Message { get; set; }
+}
