96c6913ae1e73417b8e3b9c162046bfbdd9a2723
phoen17x <olegerokhinp@gmail.com>
2023-02-24 18:56:19 +0300

    feat: add ModelValidator

---
+++ Shared/DailyPlanner.Common/Validator/IModelValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace DailyPlanner.Common.Validator;
+
+/// <summary>
+/// Defines the contract for validating an object of type T.
+/// </summary>
+/// <typeparam name="T">The type of object to validate.</typeparam>
+public interface IModelValidator<T> where T : class
+{
+    /// <summary>
+    /// Validates the given model of type T.
+    /// </summary>
+    /// <param name="model">The model to validate.</param>
+    void Check(T model);
+}
---
+++ Shared/DailyPlanner.Common/Validator/ModelValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+
+namespace DailyPlanner.Common.Validator;
+
+/// <summary>
+/// Provides a generic implementation of the <see cref="IModelValidator{T}"/>.
+/// </summary>
+/// <typeparam name="T">The type of model to validate.</typeparam>
+public class ModelValidator<T> : IModelValidator<T> where T : class
+{
+    private readonly IValidator<T> validator;
+
+    public ModelValidator(IValidator<T> validator)
+    {
+        this.validator = validator;
+    }
+
+    /// <summary>
+    /// Validates the specified model and throws a <see cref="ValidationException"/> if the model is invalid.
+    /// </summary>
+    /// <param name="model">The model to validate.</param>
+    /// <exception cref="ValidationException">Thrown if the model is invalid.</exception>
+    public void Check(T model)
+    {
+        var validationResult = validator.Validate(model);
+        if (validationResult.IsValid == false)
+            throw new ValidationException(validationResult.Errors);
+    }
+}
