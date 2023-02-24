7f86c35891759b3264e0a5da957025b0e5fed453
phoen17x <olegerokhinp@gmail.com>
2023-02-24 19:59:54 +0300

    feat: configure AutoMapper

---
+++ Systems/Api/DailyPlanner.Api/Configuration/AutoMapperConfiguration.cs
@@ -0,0 +1,22 @@
+﻿namespace DailyPlanner.Api.Configuration;
+
+/// <summary>
+/// The AutoMapper configuration.
+/// </summary>
+public static class AutoMapperConfiguration
+{
+    /// <summary>
+    /// Adds AutoMapper to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection to add AutoMapper to.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddAppAutoMapper(this IServiceCollection services)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(assembly => assembly.FullName != null && assembly.FullName.ToLower().StartsWith("dailyplanner"));
+
+        services.AddAutoMapper(assemblies);
+
+        return services;
+    }
+}
