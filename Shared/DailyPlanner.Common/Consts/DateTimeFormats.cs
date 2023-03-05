7551ee5c6525906040897557fc801fda1530e01e
phoen17x <olegerokhinp@gmail.com>
2023-03-05 17:14:32 +0300

    feat: add datetime consts

---
+++ Shared/DailyPlanner.Common/Consts/DateTimeFormats.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPlanner.Common.Consts;
+
+/// <summary>
+/// Provides constants for various date and time formats.
+/// </summary>
+public static class DateTimeFormats
+{
+    public const string DATE_TIME_WITHOUT_SECONDS = "dd/MM/yyyy HH:mm";
+}
