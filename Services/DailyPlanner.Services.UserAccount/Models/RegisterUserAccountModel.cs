d2a3b6804a711b3af24ccdea18c5d6ac48e42a2a
phoen17x <olegerokhinp@gmail.com>
2023-03-12 12:28:16 +0300

    feat: add user models

---
+++ Services/DailyPlanner.Services.UserAccount/Models/RegisterUserAccountModel.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+
+namespace DailyPlanner.Services.UserAccount.Models;
+
+/// <summary>
+/// Represents the model used to register a new user.
+/// </summary>
+public class RegisterUserAccountModel
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
+
+/// <summary>
+/// Validates the <see cref="RegisterUserAccountModel"/>.
+/// </summary>
+public class RegisterUserAccountModelValidator : AbstractValidator<RegisterUserAccountModel>
+{
+    public RegisterUserAccountModelValidator()
+    {
+        RuleFor(model => model.Name)
+            .NotEmpty().WithMessage("Username is required.")
+            .MaximumLength(16).WithMessage("Username is too long.")
+            .MinimumLength(3).WithMessage("Username is too short.");
+
+        RuleFor(model => model.Email)
+            .EmailAddress().WithMessage("Email is required.");
+
+        RuleFor(model => model.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MaximumLength(50).WithMessage("Password is too long.")
+            .MinimumLength(8).WithMessage("Password is too short.");
+    }
+}
---
+++ Services/DailyPlanner.Services.UserAccount/Models/UserAccountModel.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DailyPlanner.Context.Entities.User;
+
+namespace DailyPlanner.Services.UserAccount.Models;
+
+/// <summary>
+/// Represents a model for a user.
+/// </summary>
+public class UserAccountModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
+
+/// <summary>
+/// Maps <see cref="User"/> to <see cref="UserAccountModel"/>.
+/// </summary>
+public class UserAccountModelProfile : Profile
+{
+    public UserAccountModelProfile()
+    {
+        CreateMap<User, UserAccountModel>();
+    }
+}
