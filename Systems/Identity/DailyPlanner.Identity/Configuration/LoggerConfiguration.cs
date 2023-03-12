8f7210371c06dc6d0ce47dabb8ed3726edf08696
phoen17x <olegerokhinp@gmail.com>
2023-03-12 11:37:19 +0300

    feat: add logger configuration

---
+++ Systems/Identity/DailyPlanner.Identity/Configuration/LoggerConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+
+namespace DailyPlanner.Identity.Configuration;
+
+/// <summary>
+/// The logger configuration.
+/// </summary>
+public static class LoggerConfiguration
+{
+    /// <summary>
+    /// Adds the logger configuration to the application.
+    /// </summary>
+    /// <param name="builder">The web application builder instance.</param>
+    public static void AddAppLogger(this WebApplicationBuilder builder)
+    {
+        var logger = new Serilog.LoggerConfiguration()
+            .Enrich.WithCorrelationIdHeader()
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+
+        builder.Host.UseSerilog(logger, true);
+    }
+}
