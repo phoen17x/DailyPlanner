3384d316deadb79adbd47a54b8bbaf1e65562816
phoen17x <olegerokhinp@gmail.com>
2023-03-12 11:32:53 +0300

    feat: add app scopes

---
+++ Shared/DailyPlanner.Common/Security/AppScopes.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPlanner.Common.Security;
+
+/// <summary>
+/// Contains the application's authorization scopes.
+/// </summary>
+public static class AppScopes
+{
+    public const string PlannerAccess = "planner_access";
+}
