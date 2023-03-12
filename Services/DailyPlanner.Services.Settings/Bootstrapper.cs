468c61e574fd563a7ae01e923dfb8dc92e55fbfb
phoen17x <olegerokhinp@gmail.com>
2023-03-12 12:23:20 +0300

    feat: add identity settings

--- Services/DailyPlanner.Services.Settings/Bootstrapper.cs
+++ Services/DailyPlanner.Services.Settings/Bootstrapper.cs
@@ -22,4 +22,18 @@ public static class Bootstrapper
 
         return services;
     }
+
+    /// <summary>
+    /// Adds identity settings to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection instance.</param>
+    /// <param name="configuration">Optional configuration to load settings from.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddIdentitySettings(this IServiceCollection services, IConfiguration? configuration = null)
+    {
+        var settings = Settings.Load<IdentitySettings>("Identity", configuration);
+        services.AddSingleton(settings!);
+
+        return services;
+    }
 }
---
+++ Services/DailyPlanner.Services.Settings/IdentitySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPlanner.Services.Settings;
+
+/// <summary>
+/// Represents identity settings.
+/// </summary>
+public class IdentitySettings
+{
+    public string Url { get; private set; }
+}
