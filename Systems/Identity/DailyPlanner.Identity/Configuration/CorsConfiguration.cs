c56d39739767759646cdf7287f6ea36bd7cc4f34
phoen17x <olegerokhinp@gmail.com>
2023-03-12 11:41:26 +0300

    feat: add cors configuration

---
+++ Systems/Identity/DailyPlanner.Identity/Configuration/CorsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Duende.IdentityServer.Services;
+
+namespace DailyPlanner.Identity.Configuration;
+
+/// <summary>
+/// The CORS configuration.
+/// </summary>
+public static class CorsConfiguration
+{
+    /// <summary>
+    /// Adds CORS services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The service collection instance.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddAppCors(this IServiceCollection services)
+    {
+        services.AddSingleton<ICorsPolicyService>(serviceProvider =>
+        {
+            var logger = serviceProvider.GetRequiredService<ILogger<DefaultCorsPolicyService>>();
+            return new DefaultCorsPolicyService(logger) { AllowAll = true };
+        });
+
+        return services;
+    }
+}
