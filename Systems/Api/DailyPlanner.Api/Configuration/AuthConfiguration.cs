423e67c7e03aa4c54467e7169ba025314416f677
phoen17x <olegerokhinp@gmail.com>
2023-03-12 12:39:22 +0300

    feat: add auth configuration

---
+++ Systems/Api/DailyPlanner.Api/Configuration/AuthConfiguration.cs
@@ -0,0 +1,83 @@
+﻿using DailyPlanner.Common.Security;
+using DailyPlanner.Context;
+using DailyPlanner.Context.Entities.User;
+using DailyPlanner.Services.Settings;
+using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace DailyPlanner.Api.Configuration;
+
+/// <summary>
+/// The auth configuration.
+/// </summary>
+public static class AuthConfiguration
+{
+    /// <summary>
+    /// Adds authentication services to the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+    /// <param name="settings">The identity settings to be used for authentication.</param>
+    /// <returns>The <see cref="IServiceCollection"/>.</returns>
+    public static IServiceCollection AddAppAuth(this IServiceCollection services, IdentitySettings? settings)
+    {
+        IdentityModelEventSource.ShowPII = true;
+
+        services
+            .AddIdentity<User, IdentityRole<Guid>>(opt =>
+            {
+                opt.Password.RequiredLength = 8;
+                opt.Password.RequireDigit = true;
+                opt.Password.RequireLowercase = true;
+                opt.Password.RequireUppercase = true;
+                opt.Password.RequireNonAlphanumeric = true;
+            })
+            .AddEntityFrameworkStores<DailyPlannerContext>()
+            .AddUserManager<UserManager<User>>()
+            .AddDefaultTokenProviders();
+
+        services
+            .AddAuthentication(options =>
+            {
+                options.DefaultScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(IdentityServerAuthenticationDefaults.AuthenticationScheme, options =>
+            {
+                options.RequireHttpsMetadata = settings?.Url.StartsWith("https://") ?? false;
+                options.Authority = settings?.Url;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = false,
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+                options.Audience = "api";
+            });
+
+
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy(AppScopes.PlannerAccess, policy => policy.RequireClaim("scope", AppScopes.PlannerAccess));
+        });
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds authentication and authorization middleware to the specified <see cref="IApplicationBuilder"/>.
+    /// </summary>
+    /// <param name="app">The <see cref="IApplicationBuilder"/> to add the middleware to.</param>
+    /// <returns>The <see cref="IApplicationBuilder"/>.</returns>
+    public static IApplicationBuilder UseAppAuth(this IApplicationBuilder app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+        return app;
+    }
+}
