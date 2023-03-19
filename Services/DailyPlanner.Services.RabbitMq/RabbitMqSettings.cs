33a431fe9fdef21d6972f2fdc7e83a601144cfaa
phoen17x <olegerokhinp@gmail.com>
2023-03-19 21:37:48 +0300

    feat: add RabbitMQ settings

---
+++ Services/DailyPlanner.Services.RabbitMq/RabbitMqSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace DailyPlanner.Services.RabbitMq;
+
+/// <summary>
+/// Represents the RabbitMQ settings.
+/// </summary>
+public class RabbitMqSettings
+{
+    public string Uri { get; private set; }
+    public string UserName { get; private set; }
+    public string Password { get; private set; }
+}
