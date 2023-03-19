551ce7bb78b1cdc4df64cd476e029b7998d95d78
phoen17x <olegerokhinp@gmail.com>
2023-03-19 21:25:34 +0300

    feat: add email model

---
+++ Services/DailyPlanner.Services.EmailSender/Models/EmailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DailyPlanner.Services.EmailSender.Models;
+
+/// <summary>
+/// Represents an email model.
+/// </summary>
+public class EmailModel
+{
+    public string Email { get; set; }
+    public string Subject { get; set; }
+    public string Message { get; set; }
+}
