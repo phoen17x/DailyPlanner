7fe67b4a056a7567cc48d04bf3cfa40a923fd953
phoen17x <olegerokhinp@gmail.com>
2023-03-19 21:23:26 +0300

    feat: add queue consts

---
+++ Shared/DailyPlanner.Common/Consts/RabbitMqQueueNames.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPlanner.Common.Consts;
+
+/// <summary>
+/// Provides constants for various RabbitMQ queue names.
+/// </summary>
+public static class RabbitMqQueueNames
+{
+    public const string SEND_EMAIL = "SEND_EMAIL";
+}
