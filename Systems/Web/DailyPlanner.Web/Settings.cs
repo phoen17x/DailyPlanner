1a0deab0e9e921bf9dcbd708062c8f72b3f44f4c
phoen17x <olegerokhinp@gmail.com>
2023-03-26 21:10:06 +0300

    feat: add settings

---
+++ Systems/Web/DailyPlanner.Web/Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPlanner.Web;
+
+public static class Settings
+{
+    public static string ApiRoot = "http://localhost:10000/api";
+    public static string IdentityRoot = "http://localhost:10001";
+    public static string ClientId = "frontend";
+    public static string ClientSecret = "secret";
+}
