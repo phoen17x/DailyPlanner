f8f2bc9467961f6e246b48675388507cc048e9b6
phoen17x <olegerokhinp@gmail.com>
2023-03-26 21:17:52 +0300

    feat: add configuration service

---
+++ Systems/Web/DailyPlanner.Web/Services/ConfigurationService.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+
+namespace DailyPlanner.Web.Services;
+
+public class ConfigurationService : IConfigurationService
+{
+    private readonly ILocalStorageService localStorage;
+
+    public ConfigurationService(ILocalStorageService localStorage)
+    {
+        this.localStorage = localStorage;
+    }
+
+    public async Task<bool> GetDarkMode()
+    {
+        return await localStorage.GetItemAsync<bool>("darkMode");
+    }
+
+    public async Task SetDarkMode(bool value)
+    {
+        await localStorage.SetItemAsync("darkMode", value);
+    }
+
+    public async Task<bool> GetNavigationMenuVisible()
+    {
+        return await localStorage.GetItemAsync<bool>("navigationMenuVisible");
+    }
+
+    public async Task SetNavigationMenuVisible(bool value)
+    {
+        await localStorage.SetItemAsync("navigationMenuVisible", value);
+    }
+}
---
+++ Systems/Web/DailyPlanner.Web/Services/IConfigurationService.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyPlanner.Web.Services;
+
+public interface IConfigurationService
+{
+    Task<bool> GetDarkMode();
+    Task SetDarkMode(bool value);
+
+    Task<bool> GetNavigationMenuVisible();
+    Task SetNavigationMenuVisible(bool value);
+}
