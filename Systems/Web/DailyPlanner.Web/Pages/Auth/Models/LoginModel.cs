55b6ad833fe255bc82b53a1f120a7e2c9ce4a16e
phoen17x <olegerokhinp@gmail.com>
2023-03-26 21:19:10 +0300

    feat: add login model and result

---
+++ Systems/Web/DailyPlanner.Web/Pages/Auth/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DailyPlanner.Web.Pages.Auth.Models;
+
+public class LoginModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+}
