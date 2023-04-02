b6de9879db5e17b7bec6fcc0095a06e2e16267aa
phoen17x <olegerokhinp@gmail.com>
2023-04-02 22:12:52 +0300

    feat: add signup models

---
+++ Systems/Web/DailyPlanner.Web/Pages/Accounts/Models/SignUpModel.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System.ComponentModel.DataAnnotations;
+
+namespace DailyPlanner.Web.Pages.Accounts.Models;
+
+public class SignUpModel
+{
+    [Required(ErrorMessage = "Name is required.")]
+    [MinLength(3, ErrorMessage = "Name must be at least 3 characters long.")]
+    [MaxLength(16, ErrorMessage = "Name can not be more than 16 characters long.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email is invalid.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required.")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long.")]
+    [MaxLength(50, ErrorMessage = "Password can not be more than 50 characters long.")]
+    public string Password { get; set; }
+}
