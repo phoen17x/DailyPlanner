e96884f148c81b9839448ac6b01ca6c733d8a324
phoen17x <olegerokhinp@gmail.com>
2023-04-02 22:23:53 +0300

    feat: add DateTime json converter

---
+++ Systems/Web/DailyPlanner.Web/JsonConverters/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace DailyPlanner.Web.JsonConverters;
+
+public class DateTimeConverter : JsonConverter<DateTime>
+{
+    private readonly string format;
+
+    public DateTimeConverter(string format)
+    {
+        this.format = format;
+    }
+
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var date = reader.GetString();
+        return date is null ? DateTime.MinValue : DateTime.ParseExact(date, format, CultureInfo.InvariantCulture);
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString(format));
+    }
+}
