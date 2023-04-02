a5e288f586700584581531ad51f9c2fe6d2781a2
phoen17x <olegerokhinp@gmail.com>
2023-04-02 22:19:33 +0300

    fix: add try catch block

--- Systems/Web/DailyPlanner.Web/Providers/ApiAuthenticationStateProvider.cs
+++ Systems/Web/DailyPlanner.Web/Providers/ApiAuthenticationStateProvider.cs
@@ -48,31 +48,39 @@ public class ApiAuthenticationStateProvider : AuthenticationStateProvider
     {
         var claims = new List<Claim>();
         var payload = jwt.Split('.')[1];
-        var jsonBytes = ParseBase64WithoutPadding(payload);
-        var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
 
-        keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
-
-        if (roles != null)
+        try
         {
-            if (roles.ToString().Trim().StartsWith("["))
-            {
-                var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
+            var jsonBytes = ParseBase64WithoutPadding(payload);
+            var keyValuePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
 
-                foreach (var parsedRole in parsedRoles)
+            keyValuePairs.TryGetValue(ClaimTypes.Role, out object roles);
+
+            if (roles != null)
+            {
+                if (roles.ToString().Trim().StartsWith("["))
                 {
-                    claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                    var parsedRoles = JsonSerializer.Deserialize<string[]>(roles.ToString());
+
+                    foreach (var parsedRole in parsedRoles)
+                    {
+                        claims.Add(new Claim(ClaimTypes.Role, parsedRole));
+                    }
                 }
-            }
-            else
-            {
-                claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
+                else
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, roles.ToString()));
+                }
+
+                keyValuePairs.Remove(ClaimTypes.Role);
             }
 
-            keyValuePairs.Remove(ClaimTypes.Role);
+            claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
         }
-
-        claims.AddRange(keyValuePairs.Select(kvp => new Claim(kvp.Key, kvp.Value.ToString())));
+        catch (Exception ex)
+        {
+            return new List<Claim>();
+        }
 
         return claims;
     }
