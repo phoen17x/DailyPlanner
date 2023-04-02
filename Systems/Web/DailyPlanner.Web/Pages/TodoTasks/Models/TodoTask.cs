be65fc0cca6eff9e0639806edb4cd4110b1c96a1
phoen17x <olegerokhinp@gmail.com>
2023-04-02 22:24:15 +0300

    feat: add todotask models

---
+++ Systems/Web/DailyPlanner.Web/Pages/TodoTasks/Models/TodoTask.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyPlanner.Web.Pages.TodoTasks.Models;
+
+public class TodoTask
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EstimatedCompletionTime { get; set; }
+    public DateTime ActualCompletionTime { get; set; }
+    public TodoTaskStatus Status { get; set; }
+    public int NotebookId { get; set; }
+}
+
+public enum TodoTaskStatus
+{
+    Scheduled,
+    Pending,
+    Completed,
+    Cancelled
+}
---
+++ Systems/Web/DailyPlanner.Web/Pages/TodoTasks/Models/TodoTaskModel.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+
+namespace DailyPlanner.Web.Pages.TodoTasks.Models;
+
+public class TodoTaskModel
+{
+    public int? Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime StartTime { get; set; }
+    public DateTime EstimatedCompletionTime { get; set; }
+    public DateTime ActualCompletionTime { get; set; }
+    public TodoTaskStatus Status { get; set; }
+    public int NotebookId { get; set; }
+}
+
+public class NotebookModelValidator : AbstractValidator<TodoTaskModel>
+{
+    public NotebookModelValidator()
+    {
+        RuleFor(model => model.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(50).WithMessage("Title is too long.");
+
+        RuleFor(model => model.Description)
+            .MaximumLength(200).WithMessage("Description is too long.");
+
+        RuleFor(model => model.StartTime)
+            .NotEmpty().WithMessage("Start time is required.");
+
+        RuleFor(model => model.EstimatedCompletionTime)
+            .NotEmpty().WithMessage("Estimated completion time is required.");
+
+        RuleFor(model => model.NotebookId)
+            .NotEmpty().WithMessage("Notebook is required.");
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<TodoTaskModel>.CreateWithOptions((TodoTaskModel)model, options => options.IncludeProperties(propertyName)));
+        return result.IsValid ? Array.Empty<string>() : result.Errors.Select(e => e.ErrorMessage);
+    };
+}
