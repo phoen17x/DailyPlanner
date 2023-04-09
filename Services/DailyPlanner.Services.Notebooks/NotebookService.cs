88258a295dda9d66542697bf013acfda12e92a69
phoen17x <olegerokhinp@gmail.com>
2023-04-09 21:21:51 +0300

    feat: add cache

--- Services/DailyPlanner.Services.Notebooks/NotebookService.cs
+++ Services/DailyPlanner.Services.Notebooks/NotebookService.cs
@@ -3,6 +3,7 @@ using DailyPlanner.Common.Exceptions;
 using DailyPlanner.Common.Validator;
 using DailyPlanner.Context;
 using DailyPlanner.Context.Entities;
+using DailyPlanner.Services.Cache;
 using DailyPlanner.Services.Notebooks.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,7 @@ public class NotebookService : INotebookService
 {
     private readonly IDbContextFactory<DailyPlannerContext> contextFactory;
     private readonly IMapper mapper;
+    private readonly ICacheService cacheService;
     private readonly IModelValidator<AddNotebookModel> addNotebookModelValidator;
     private readonly IModelValidator<UpdateNotebookModel> updateNotebookModelValidator;
 
@@ -23,16 +25,19 @@ public class NotebookService : INotebookService
     /// </summary>
     /// <param name="contextFactory">Factory that provides access to the <see cref="DailyPlannerContext"/>.</param>
     /// <param name="mapper">Object mapper that maps entities to models and vice versa.</param>
+    /// <param name="cacheService">Service to cache data.</param>
     /// <param name="addNotebookModelValidator">Validator for <see cref="AddNotebookModel"/>.</param>
     /// <param name="updateNotebookModelValidator">Validator for <see cref="UpdateNotebookModel"/>.</param>
     public NotebookService(
         IDbContextFactory<DailyPlannerContext> contextFactory,
         IMapper mapper,
+        ICacheService cacheService,
         IModelValidator<AddNotebookModel> addNotebookModelValidator,
         IModelValidator<UpdateNotebookModel> updateNotebookModelValidator)
     {
         this.contextFactory = contextFactory;
         this.mapper = mapper;
+        this.cacheService = cacheService;
         this.addNotebookModelValidator = addNotebookModelValidator;
         this.updateNotebookModelValidator = updateNotebookModelValidator;
     }
@@ -40,6 +45,10 @@ public class NotebookService : INotebookService
 
     public async Task<IEnumerable<NotebookModel>> GetNotebooks(Guid userId, int offset = 0, int limit = 10)
     {
+        var cacheKey = $"dailyplanner:notebooks-{userId}";
+        var cachedData = await cacheService.Get<IEnumerable<NotebookModel>>(cacheKey);
+        if (cachedData is not null) return cachedData;
+
         await using var context = await contextFactory.CreateDbContextAsync();
         var notebooks = context.Notebooks.AsQueryable();
         notebooks = notebooks
@@ -48,6 +57,7 @@ public class NotebookService : INotebookService
             .Take(Math.Max(0, Math.Min(limit, 1000)));
 
         var data = (await notebooks.ToListAsync()).Select(mapper.Map<NotebookModel>);
+        await cacheService.Put(cacheKey, data);
 
         return data;
     }
@@ -73,6 +83,9 @@ public class NotebookService : INotebookService
         await context.Notebooks.AddAsync(notebook);
         await context.SaveChangesAsync();
 
+        var cacheKey = $"dailyplanner:notebooks-{model.UserId}";
+        await cacheService.Delete(cacheKey);
+
         return mapper.Map<NotebookModel>(notebook);
     }
 
@@ -88,6 +101,10 @@ public class NotebookService : INotebookService
 
         notebook = mapper.Map(model, notebook);
         context.Notebooks.Update(notebook!);
+
+        var cacheKey = $"dailyplanner:notebooks-{model.UserId}";
+        await cacheService.Delete(cacheKey);
+
         await context.SaveChangesAsync();
     }
 
@@ -101,6 +118,10 @@ public class NotebookService : INotebookService
         ProcessException.ThrowIfNull(notebook, $"The notebook with id {notebookId} was not found.");
 
         context.Remove(notebook!);
+
+        var cacheKey = $"dailyplanner:notebooks-{userId}";
+        await cacheService.Delete(cacheKey);
+
         await context.SaveChangesAsync();
     }
 }
