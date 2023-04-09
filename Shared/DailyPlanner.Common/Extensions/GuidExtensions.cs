c79838435706b62f7c93fec9a0185f4958dc559d
phoen17x <olegerokhinp@gmail.com>
2023-04-09 21:23:50 +0300

    feat: add shrink guid extension

---
+++ Shared/DailyPlanner.Common/Extensions/GuidExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyPlanner.Common.Extensions;
+
+public static class GuidExtensions
+{
+    public static string Shrink(this Guid id)
+    {
+        return id.ToString().Replace("-", "").Replace(" ", "");
+    }
+}
