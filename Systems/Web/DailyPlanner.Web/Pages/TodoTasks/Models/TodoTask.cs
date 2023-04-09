422fcd5ba47a9b87bfb629f175c27cfebd8d1a2d
phoen17x <olegerokhinp@gmail.com>
2023-04-09 21:30:35 +0300

    feat: add DateTime to string functions

--- Systems/Web/DailyPlanner.Web/Pages/TodoTasks/Models/TodoTask.cs
+++ Systems/Web/DailyPlanner.Web/Pages/TodoTasks/Models/TodoTask.cs
@@ -1,4 +1,6 @@
-﻿namespace DailyPlanner.Web.Pages.TodoTasks.Models;
+﻿using System.Text;
+
+namespace DailyPlanner.Web.Pages.TodoTasks.Models;
 
 public class TodoTask
 {
@@ -10,7 +12,41 @@ public class TodoTask
     public DateTime ActualCompletionTime { get; set; }
     public TodoTaskStatus Status { get; set; }
     public int NotebookId { get; set; }
-}
+
+    public string StartTimeToString()
+    {
+        if (DateTime.Today.ToShortDateString() == StartTime.ToShortDateString())
+            return $"Starts today at {StartTime:HH:mm}";
+
+        if (DateTime.Today.AddDays(1).ToShortDateString() == StartTime.ToShortDateString())
+            return $"Starts tomorrow at {StartTime:HH:mm}";
+
+        return $"Starts {StartTime.ToString("MMMM d, yyyy")} at {StartTime:HH:mm}";
+    }
+
+    public string DateTimeToString()
+    {
+        if (StartTime.ToShortDateString() == EstimatedCompletionTime.ToShortDateString())
+        {
+            if (DateTime.Today.ToShortDateString() == StartTime.ToShortDateString())
+                return $"Today {StartTime:HH:mm} - {EstimatedCompletionTime:HH:mm}";
+            
+            if (DateTime.Today.AddDays(1).ToShortDateString() == StartTime.ToShortDateString())
+                return $"Tomorrow {StartTime:HH:mm} - {EstimatedCompletionTime:HH:mm}";
+
+            return $"{StartTime.ToString("d MMMM yyyy")} {StartTime:HH:mm} - {EstimatedCompletionTime:HH:mm}";
+        }
+
+        if (DateTime.Today.ToShortDateString() == StartTime.ToShortDateString() &&
+            DateTime.Today.AddDays(1).ToShortDateString() == StartTime.ToShortDateString())
+            return $"Today {StartTime:HH:mm} - Tomorrow {EstimatedCompletionTime:HH:mm}";
+
+        if (DateTime.Today.AddDays(1).ToShortDateString() == StartTime.ToShortDateString())
+            return $"Tomorrow {StartTime:HH:mm} - {EstimatedCompletionTime.ToString("d MMMM yyyy")} {EstimatedCompletionTime:HH:mm}";
+
+        return $"{StartTime.ToString("d MMMM yyyy")} {StartTime:HH:mm} - {EstimatedCompletionTime.ToString("d MMMM yyyy")} {EstimatedCompletionTime:HH:mm}";
+    }
+}   
 
 public enum TodoTaskStatus
 {
