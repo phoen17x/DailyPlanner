56e84fcbc84c9c9a531f08c8524e08abab57b053
phoen17x <olegerokhinp@gmail.com>
2023-04-29 23:59:06 +0300

    feat: add user model

---
+++ Systems/Web/DailyPlanner.Web/Pages/Accounts/Models/UserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DailyPlanner.Web.Pages.Accounts.Models;
+
+public class UserModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
